689c6e3a2548bbe7682a1be0fe22027d453d65ce
Vincent <vkammerer@gmail.com>
2015-12-01 17:52:47 -0500

    Install teleporter and set navigation animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,42 @@ import AppCss from './css/App.css';
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.flipExpand = this.flipExpand.bind(this)
-		this.flipReduce = this.flipReduce.bind(this)
 		this.state = {
-			isFlipped: false
+			selectedRoom: ''
 		}
 	}
-	flipExpand() {
-		this.setState({isFlipped: !this.state.isFlipped});
-	}
-	flipReduce() {
-		this.setState({isFlipped: false});
+	roomSelection(roomName) {
+		console.log(roomName)
+		this.setState({selectedRoom: roomName});
 	}
 	render() {
+		let rooms = {
+			selectedRoom: this.state.selectedRoom,
+			roomSelection: this.roomSelection.bind(this)
+		}
 		return (
 			<div
 				className={AppCss.app}>
-				<Room>
-					<div
-						onClick={this.flipExpand}>
-						<div>expand</div>
-						<div>reduce</div>
+				<Room
+					rooms={rooms}
+					room={{
+						name: 'cuisine',
+						top: 20,
+						left: 20
+					}}>
+					<div>
+						Cuisine
+					</div>
+				</Room>
+				<Room
+					rooms={rooms}
+					room={{
+						name: 'garage',
+						top: 10,
+						left: 0
+					}}>
+					<div>
+						Garage
 					</div>
 				</Room>
 			</div>
--- src/Room.jsx
+++ src/Room.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
+import classNames from 'classnames';
+import Teleporter from 'teleporter';
 import RoomCss from './css/Room.css';
 
 export default class Room extends React.Component {
 	constructor(props) {
 		super(props);
-		this.openPreview = this.openPreview.bind(this)
+		this.expand = this.expand.bind(this);
+		this.contract = this.contract.bind(this);
 	}
-	openPreview() {
-		console.log(this);
+	expand(){
+		if (this.props.rooms.selectedRoom !== this.props.room.name) {
+			this.props.rooms.roomSelection(this.props.room.name);
+		}
+	}
+	contract(){
+		if (this.props.rooms.selectedRoom === this.props.room.name) {
+			this.props.rooms.roomSelection('');
+		}
+	}
+	componentDidMount() {
+		this.teleporter = new Teleporter({
+			selector: `#${this.refs.roomRoot.id}`,
+			sizeClass: RoomCss.expanded
+		})
+	}
+	componentDidUpdate(prevProps){
+		if (
+			(prevProps.rooms.selectedRoom !== this.props.rooms.selectedRoom) &&
+			(
+				(prevProps.rooms.selectedRoom === this.props.room.name) ||
+				(this.props.rooms.selectedRoom === this.props.room.name)
+			)
+		) {
+			let steps = this.props.rooms.selectedRoom === this.props.room.name ?
+				['', RoomCss.expanded] : [RoomCss.expanded, ''];
+			this.teleporter.teleport(steps);
+		}
 	}
 	render() {
+		let roomClass = classNames({
+			[RoomCss.common]: true,
+			[RoomCss.selected]: this.props.rooms.selectedRoom === this.props.room.name
+		});
 		return (
-			<div className={RoomCss.common}>
-				<div className={RoomCss.again}>
-					${this.props.children}
+			<div
+				ref='roomRoot'
+				id={this.props.room.name}
+				className={roomClass}
+				style={{
+					top: `${this.props.room.top}%`,
+					left: `${this.props.room.left}%`
+				}}>
+				<div>
+					<div
+						onClick={this.expand}
+						className={RoomCss.expand}>
+						Expand
+					</div>
+					<div
+						onClick={this.contract}
+						className={RoomCss.contract}>
+						Contract
+					</div>
+					{this.props.children}
 				</div>
 			</div>
 		);
