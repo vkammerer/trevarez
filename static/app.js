7b45ed471a3999fbc7dfe27378fb0cd952912c12
xseignard <xavier.seignard@gmail.com>
2015-12-07 17:08:09 +0100

    Upgraded to electron 0.35.4 and shortcuts in electron app

--- static/app.js
+++ static/app.js
@@ -1,5 +1,7 @@
-var app = require('app'),
-	BrowserWindow = require('browser-window');
+var electron = require('electron'),
+	app = require('app'),
+	BrowserWindow = require('browser-window'),
+	globalShortcut = electron.globalShortcut;
 
 require('crash-reporter').start();
 
@@ -10,6 +12,23 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 	mainWindow = new BrowserWindow({ fullscreen: true });
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	if (process.platform === 'darwin') {
+		globalShortcut.register('Alt+Command+I', function() {
+			mainWindow.toggleDevTools();
+		});
+		globalShortcut.register('Comamnd+Shift+F', function() {
+			mainWindow.setFullScreen(!mainWindow.isFullScreen());
+		});
+	}
+	else {
+		globalShortcut.register('Alt+Ctrl+I', function() {
+			mainWindow.toggleDevTools();
+		});
+		globalShortcut.register('F11', function() {
+			mainWindow.setFullScreen(!mainWindow.isFullScreen());
+		});
+	}
 
 	// closing strategy
 	mainWindow.on('closed', function() {
